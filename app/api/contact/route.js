1120b05dba0046c92ef42fc11f9456636acf7ff5
mdjawedansari <116138541+mdjawedansari@users.noreply.github.com>
2024-05-30 14:19:43 +0530

    Update route.js

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -8,17 +8,21 @@ export async function POST(req) {
     const formData = await req.json();
 
     // Save form data to MongoDB
-    // const client = await clientPromise;
-    // const db = client.db();
-    // const contact =  await db.collection('contacts').insertOne(formData);
-    // if(!contact.insertedId){
-    //     throw new Error('contact not found !');
-    // }
+    const client = await clientPromise;
+    const db = client.db('portfolio');
+    const contact =  await db.collection('contacts').insertOne(formData);
+    if(!contact.insertedId){
+        throw new Error('contact not found !');
+    } else {
+        console.log('Contact successfully inserted with ID:', contact.insertedId);
+      }
 
     await sendMail(formData);
     return NextResponse.json({ success: true, message: 'Form submitted successfully' });
   } catch (error) {
     console.error('Error submitting form:', error);
     return NextResponse.json({ success: false, message: 'Failed to submit form', error }, { status: 500 });
+  } finally {
+    client.close(); 
   }
 }
