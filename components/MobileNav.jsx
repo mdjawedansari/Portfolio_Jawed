c1cf9be51d021463b7fb951e5b272c038a32ba3d
mdjawedansari <116138541+mdjawedansari@users.noreply.github.com>
2024-06-05 11:58:24 +0530

    Update MobileNav.jsx

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { CiMenuFries } from "react-icons/ci";
@@ -42,45 +42,48 @@ const MobileNav = () => {
 
   return (
     <>
-    
-      <Sheet isOpen={isOpen} onOpenChange={setIsOpen}>
-        <SheetTrigger className="flex justify-center items-center">
-          <CiMenuFries className="text-[32px] text-accent" onClick={() => setIsOpen(!isOpen)}/>
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger asChild>
+        <button className="flex justify-center items-center" onClick={() => setIsOpen(!isOpen)}>
+            <CiMenuFries className="text-[32px] text-accent" />
+          </button>
         </SheetTrigger>
         {isOpen && (
-        <ScrollArea>
-          <SheetContent className="flex flex-col">
-            <div className="mt-20 mb-20 ">
-              <Link href="/">
-                <h1 className="text-4xl font-primary text-center">
-                  Jawed <span className="text-accent">.</span>
-                </h1>
-              </Link>
-            </div>
-
-            <nav className="flex flex-col justify-center items-center gap-8">
-              {links.map((link, index) => (
-                <Link 
-                  onClick={handleItemClick}
-                  href={link.path}
-                  key={index}
-                  className={`${
-                    link.path === pathName && "borderb-2 border-accent"
-                  } text-xl capitalize hover:text-accent transition-all`}
-                >
-                  {link.name}
+          <ScrollArea>
+            <SheetContent className="flex flex-col">
+              <div className="mt-20 mb-20 ">
+                <Link href="/">
+                  <h1 className="text-4xl font-primary text-center">
+                    Jawed <span className="text-accent">.</span>
+                  </h1>
                 </Link>
-              ))}
-            </nav>
+              </div>
 
-            <Link href="/contact" className="mt-6 animate-pulse flex justify-center items-center">
-              <Button>Hire me</Button>
-            </Link>
-          </SheetContent>
-        </ScrollArea>
+              <nav className="flex flex-col justify-center items-center gap-8">
+                {links.map((link, index) => (
+                  <Link
+                    onClick={handleItemClick}
+                    href={link.path}
+                    key={index}
+                    className={`${
+                      link.path === pathName && "border-b-2 border-accent"
+                    } text-xl capitalize hover:text-accent transition-all`}
+                  >
+                    {link.name}
+                  </Link>
+                ))}
+              </nav>
+
+              <Link
+                href="/contact"
+                className="mt-6 animate-pulse flex justify-center items-center"
+              >
+                <Button>Hire me</Button>
+              </Link>
+            </SheetContent>
+          </ScrollArea>
         )}
       </Sheet>
-      
     </>
   );
 };
